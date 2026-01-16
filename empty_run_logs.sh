c6ee86e170a4626f220bc095dfeb3da3fec5eaa5
Christian Gintenreiter <christian.gintenreiter@gmail.com>
2026-01-16 14:13:41 +0100

    Refactor empty_run_logs.sh to use truncate for clearing CSV files instead of echo. This improves efficiency and clarity in resetting log files.

--- empty_run_logs.sh
+++ empty_run_logs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-echo -n "" > src/optimize_agent/run_logs/metrics.csv
-echo -n "" > src/optimize_agent/run_logs/runs.csv
+truncate -s 0 src/optimize_agent/run_logs/metrics.csv
+truncate -s 0 src/optimize_agent/run_logs/runs.csv
 
 rm src/optimize_agent/run_logs/*.log
 rm src/optimize_agent/run_logs/*.json
